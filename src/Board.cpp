cf72d77329edb28adcc5772952d4655990e57d68
maddca <maddca@cs.wallawalla.edu>
2020-03-12 17:15:41 +0000

    autosave

--- src/Board.cpp
+++ src/Board.cpp
@@ -9,6 +9,7 @@ using namespace std;
 //Blue is 1 red is 0;
 // Keep in mind to consider Blue to be natural bottom of the board.
 // 2d array is [rows][columns] or [y][x]
+// i is row j is column.
 void Board::printBoard() {
   cout << endl << "             || RED ||\n";
   cout << "==================================\n";
@@ -61,23 +62,20 @@ bool Board::validateMovement(Piece *piece, int x, int y, int movement,
 
 // Seperated the function of validate into a validate selection and validate
 // movement to support scout movement speed
+
 bool Board::validateSelection(int x, int y, bool playerTurn) {
   if (x < 0 || y < 0 || x > 9 || y > 9) {
     return false;
-  } else if (Pieces[y][x]->getName() == "River" ||
+  } else if (Pieces[y][x]->getMovement() == 0 ||
              playerTurn != Pieces[y][x]->getSide()) {
+    cout << "This piece cannot be moved." << endl;
     return false;
   } else {
     return true;
   }
 }
 
-// Garrett seems to be making the board with (0,0) in the top left keep that in
-// mind. Blue seems to work ok, but red needs work -CM
-// If you restrict the placement to the x and y values you have, then there's
-// no way to hit a river piece. -AP
-
-// Validate movement still needs to check if a place on the board is occupied by another piece
+//DONE
 bool Board::validatePlacement(int x, int y, bool playerTurn) {
   if (playerTurn) { // blue
     if (x >= 0 && y >= 6 && x <= 9 && y <= 9) {
@@ -114,13 +112,17 @@ Piece *Board::selectPiece(int x, int y, bool playerTurn) {
 // or y of piece how does this affect the piece's location.
 // Also, you need to consider things like the scout's movement ability, which
 // lets you select the distance you want to move in a particular direction. When
-// you do move in that direction you need to do a for loop and "move" it through
+// you do move in that direction you need to do a loop and "move" it through
 // every space to make sure there isn't anything in the way.-CM
+
+
+// select direction
+// if scout 
 void Board::move(Piece *piece, int x, int y, bool playerTurn) {
   if (validateMovement(piece, x, y, piece->getMovement(), playerTurn)) {
   }
 }
-// Fixed.
+// DONE
 void Board::place(Piece *piece) {
   Pieces[piece->getY()][piece->getX()] = piece;
 }
